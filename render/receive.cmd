1be669cbde02f6e67fca3f02434d893b3b96aadc
David J Brady <dbrady@aqueti.com>
2019-02-15 14:45:52 -0500

    Add files via upload
    This file allows one to receive a video stream via gstreamer on a windows machine. Gstreamer needs to be installed, this command file runs from the gstreamer bin directory . See https://gstreamer.freedesktop.org/documentation/installing/on-windows.html

---
+++ render/receive.cmd
@@ -0,0 +1,3 @@
+:: receive command for viewing a jpeg stream over the network  
+.\gst-launch-1.0 udpsrc port=5000 ! application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! autovideosink
+
