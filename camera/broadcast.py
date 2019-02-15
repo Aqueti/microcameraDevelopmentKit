d10b4be053b36ce6a589c7b6f045151b0b4d238f
David J Brady <dbrady@aqueti.com>
2019-02-15 13:28:45 -0500

    Update broadcast.py

--- camera/broadcast.py
+++ camera/broadcast.py
@@ -8,39 +8,52 @@
 #
 
 
-import sys
-import atexit
-import subprocess
+import sys, atexit, subprocess, getopt
 
 def printHelp():
     print("grab broadcast video from camera to remote machine")
     print("---------------------------------")
-    print("broadcast.py sensor-id remoteMachineIP streamingFormat(0 for jpeg, 1 for h.264")
+    print("broadcast.py -s <sensor-id 0 or 1> -i <'remoteMachineIP' (required)> -f <streamingFormat(0 for jpeg, 1 for h.264)> -p <port number (default 5000)>")
     print("--help or -h prints help message and exits")
 
-def main():
-        inputs=sys.argv[1:]
-        if len(inputs)==3:
-            sid=inputs[0]
-            hostip=inputs[1]
-            port=5000
-            if inputs[2]==str(0):
-                cmd="gst-launch-1.0  nvcamerasrc fpsRange=\"30.0 30.0\" sensor-id="+str(sid)+"' ! 'video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080,"
-                cmd=cmd+"format=(string)I420, framerate=(fraction)30/1'  ! nvjpegenc ! rtpjpegpay ! udpsink host=$hostip port=$port"
-                subprocess.call(cmd,shell=True)
-            elif inputs[2]==str(1):
-                fname=test.h264
-                #h264 encode+stream over network
-                cmd="gst-launch-1.0 -e nvcamerasrc num-buffers=300  fpsRange=\"30.0 30.0\" sensor-id="+str(sid)
-                cmd=cmd+"! 'video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)I420, framerate=(fraction)30/1' "
-                cmd=cmd+"! nvvidconv flip-method=2 ! 'video/x-raw(memory:NVMM), format=(string)I420' ! tee name=streams streams. ! omxh264enc ! "
-                cmd=cmd+"'video/x-h264, stream-format=(string)byte-stream' ! h264parse ! rtph264pay mtu=1400 ! udpsink host=$hostip port=$port streams. "
-                cmd=cmd+"! omxh264enc bitrate=8000000 ! 'video/x-h264, stream-format=(string)byte-stream' ! filesink location=$fname"
-                subprocess.call(cmd,shell=True)
-            else:
-                printHelp()
-        else:
-                printHelp()
+def main(argv):
+    sid=0
+    fmt=0
+    port=5000
+    try:
+      opts, args = getopt.getopt(argv,"si:fp",["sid","ip=","format","port"])
+    except getopt.GetoptError:
+      printHelp()
+      sys.exit(2)
+    for opt, arg in opts:
+      if opt == in ('-h', '--help'):
+         printHelp()
+         sys.exit()
+      elif opt in ("-s", "--sid"):
+         sid = arg
+      elif opt in ("-i", "--ip"):
+         hostip = arg
+      elif opt in ("-f", "--format"):
+         fmt = arg
+      elif opt in ("-p", "--port"):
+         port = arg
+    if fmt==0:
+        cmd="gst-launch-1.0  nvcamerasrc fpsRange=\"30.0 30.0\" sensor-id="+str(sid)+"' ! 'video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080,"
+        cmd=cmd+"format=(string)I420, framerate=(fraction)30/1'  ! nvvidconv flip-method=2 !"
+        cmd=cmd+"! nvjpegenc ! rtpjpegpay ! udpsink host= "+hostip +" port="+str(port)
+        subprocess.call(cmd,shell=True)
+    elif fmt==1:
+         fname=test.h264
+         #h264 encode+stream over network
+         cmd="gst-launch-1.0 -e nvcamerasrc num-buffers=300  fpsRange=\"30.0 30.0\" sensor-id="+str(sid)
+         cmd=cmd+"! 'video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)I420, framerate=(fraction)30/1' "
+         cmd=cmd+"! nvvidconv flip-method=2 ! 'video/x-raw(memory:NVMM), format=(string)I420' ! tee name=streams streams. ! omxh264enc ! "
+         cmd=cmd+"'video/x-h264, stream-format=(string)byte-stream' ! h264parse ! rtph264pay mtu=1400 ! udpsink host="
+         cmd=cmd+hostip+ " port="port+" streams. "
+         cmd=cmd+"! omxh264enc bitrate=8000000 ! 'video/x-h264, stream-format=(string)byte-stream' ! filesink location=$fname"
+         subprocess.call(cmd,shell=True)
+     else:
+          printHelp()
 
 
 if __name__ == '__main__':
